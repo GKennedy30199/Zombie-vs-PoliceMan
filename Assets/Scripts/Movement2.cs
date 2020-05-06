afb8c10497e3953e14f70638ce2b0909d5a3c241
gkennedy982 <gkennedy982@gmail.com>
2020-05-06 17:02:51 +0100

    Copai movement
    cops go after player

---
+++ Assets/Scripts/Movement2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2 : NavMeshMover
+{
+    public string TagToTrack = "Player";
+    GameObject trackedPlayer;
+    public float TrackDistance = 2;
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        trackedPlayer = GameObject.FindGameObjectWithTag(TagToTrack);
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(trackedPlayer!=null)
+        {
+            if(Vector3.Distance(transform.position,trackedPlayer.transform.position)<=TrackDistance)
+            MoveTo(trackedPlayer);
+        }
+    }
+    public override void OnDrawGizmos()
+    {
+        Gizmos.color = DebugLineColor;
+        Gizmos.DrawWireSphere(transform.position, TrackDistance);
+        base.OnDrawGizmos();
+    }
+}
---
+++ Assets/Scripts/NavMesh Mover.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class NavMeshMover : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    public virtual void MoveTo(Vector3 position)
+    {
+        agent.SetDestination(position);
+    }
+    public virtual void MoveTo(GameObject target)
+    {
+        MoveTo(target.transform.position);
+    }
+    public virtual void Stop()
+    {
+        agent.isStopped = true;
+    }
+    public Color DebugLineColor = Color.red;
+    public virtual void OnDrawGizmos()
+    {
+        if(agent !=null)
+        {
+            if(agent.pathStatus != NavMeshPathStatus.PathInvalid)
+            {
+                for(int i=0; i < agent.path.corners.Length;i++)
+                {
+                    if (i + 1 < agent.path.corners.Length)
+                    {
+                        Gizmos.color = DebugLineColor;
+                        Gizmos.DrawLine(agent.path.corners[i], agent.path.corners[i + 1]);
+                    }
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
